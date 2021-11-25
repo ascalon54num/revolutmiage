864185593b050dc0b4b9143d9efcda06fbf603a4
Stephane <stephane.mairesse54@gmail.com>
2021-11-25 11:54:10 +0100

    starter

---
+++ BanqueApi/src/main/java/fr/univlorraine/miage/revolutmiage/RevolutmiageApplication.java
@@ -0,0 +1,13 @@
+package fr.univlorraine.miage.revolutmiage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevolutmiageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevolutmiageApplication.class, args);
+	}
+
+}
